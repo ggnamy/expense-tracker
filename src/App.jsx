f207950650c8faa771e22193c3ce72c0665aadd0
ggnamy <Anmanya.genius2549@gmail.com>
2026-04-30 15:32:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { ExpenseProvider } from './context/ExpenseContext';
+import AddExpenseForm from './components/AddExpenseForm';
+import ExpenseSummary from './components/ExpenseSummary';
+import ExpenseList from './components/ExpenseList';
+import ExpenseChart from './components/ExpenseChart';
+
+import styles from "./App.module.css";
+import "./global.css";
+const title = import.meta.env.VITE_APP_TITLE || "Expense Tracker"
+export default function App() {
+  return (
+    <ExpenseProvider>
+      <div className={styles.appWrapper}>
+        
+        <header className={styles.appHeader}>
+          <h1>Expense Tracker</h1>
+        </header>
+
+        <div className={styles.appLayout}>
+          
+          <aside className={styles.appSidebar}>
+            <ExpenseSummary />
+            <ExpenseChart />
+          </aside>
+
+          <main className={styles.appContent}>
+            <AddExpenseForm />
+            <div className={styles.listContainer}>
+              <ExpenseList />
+            </div>
+          </main>
+
+        </div>
+
+      </div>
+    </ExpenseProvider>
+  );
+}
